f0d6c55b225038d3fa3a50f5f5011fe9d0fdd051
chinggo <chris.c.chen0216@gmail.com>
2024-06-19 09:24:42 +0800

    新增 Model

---
+++ Models/TemplateC1B.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace papacy1.Models
+{
+    public class TemplateC1B
+    {
+        public string 批號 { get; set; }
+        public string 規格 { get; set; }
+        public string 根數 { get; set; }
+        public string 箱號 { get; set; }
+        public double 淨重 { get; set; }
+        public string 日期 { get; set; }
+        public string 紗別 { get; set; }
+        public string 等級 { get; set; }
+    }
+}
---
+++ Models/TemplateC2B.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace papacy1.Models
+{
+    public class TemplateC2B
+    {
+        public string 單別單號 { get; set; }
+        public string 日期 { get; set; }
+        public string 記錄 { get; set; }
+        public string 電腦編號 { get; set; }
+        public string 批號 { get; set; }
+        public string 紗別 { get; set; }
+        public string 規格 { get; set; }
+        public string 等級 { get; set; }
+        public string 經緯別 { get; set; }
+        public string 編號 { get; set; }
+        public string 總箱數 { get; set; }
+        public string 總管數 { get; set; }
+        public string 總毛重 { get; set; }
+        public string 總淨重 { get; set; }
+        public List<Detail> 明細 { get; set; }
+
+        public class Detail
+        {
+            public string No { get; set; }
+            public string Num { get; set; }
+            public string GW { get; set; }
+            public string NW { get; set; }
+        }
+    }
+}
